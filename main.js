6ed55530b089bf78a5e00d1c514ddf0f8c1d4faf
Mikel <gusidam@gmail.com>
2021-02-27 21:57:30 +0000

    dryer JS

--- main.js
+++ main.js
@@ -4,87 +4,31 @@ function escapeHtmlString(htmlStr) {
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;");
 }
-const fetchHtml = async () => {
-  try {
-    const response = await fetch("./index.html");
-    if (response.status !== 200) {
-      throw response;
-    }
-    let data = await response.text();
-    data = escapeHtmlString(data);
-    document.getElementById(
-      "html-code-showcase",
-    ).innerHTML = `<pre><code>${data}</code></pre>`;
-    return data;
-  } catch (err) {
-    console.log("error", err);
-  }
-};
 
-const fetchStyles = async () => {
+const fetchSource = async (endPoint) => {
   try {
-    const response = await fetch("./styles/styles.scss");
+    const response = await fetch(endPoint);
     if (response.status !== 200) {
       throw response;
     }
     let data = await response.text();
     data = escapeHtmlString(data);
-    document.getElementById(
-      "css-code-showcase",
-    ).innerHTML = `<pre><code>${data}</code></pre>`;
     return data;
   } catch (err) {
     console.log("error", err);
   }
 };
-
-const fetchMain = async () => {
-  try {
-    const response = await fetch("./main.js");
-    if (response.status !== 200) {
-      throw response;
-    }
-    let data = await response.text();
-    data = escapeHtmlString(data);
-    document.getElementById(
-      "main-code-showcase",
-    ).innerHTML = `<pre><code>${data}</code></pre>`;
-    return data;
-  } catch (err) {
-    console.log("error", err);
-  }
-};
-Promise.all([fetchHtml(), fetchStyles(), fetchMain()]).then((data) => {
-  console.log(data);
+Promise.all([
+  fetchSource("./index.html"),
+  fetchSource("./styles/styles.scss"),
+  fetchSource("./main.js"),
+]).then((data) => {
+  const nodes = document.querySelectorAll(".source-code");
+  nodes.forEach((node, index) => {
+    node.innerHTML = `<pre><code>${data[index]}</code></pre>`;
+  });
+  //console.log(data, nodes);
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
   });
 });
-// let htmlCodeLoaded = false;
-// let stylesCodeLoaded = false;
-// let mainCodeLoaded = false;
-
-// document
-//   .getElementById("go-to-index-btn")
-//   .addEventListener("click", async () => {
-//     if (!htmlCodeLoaded) {
-//       fetchHtml();
-//       htmlCodeLoaded = true;
-//     }
-//   });
-// document
-//   .getElementById("go-to-styles-btn")
-//   .addEventListener("click", async () => {
-//     if (!stylesCodeLoaded) {
-//       fetchStyles();
-//       stylesCodeLoaded = true;
-//     }
-//   });
-// document
-//   .getElementById("go-to-main-btn")
-//   .addEventListener("click", async () => {
-//     if (!mainCodeLoaded) {
-//       fetchMain();
-//       mainCodeLoaded = true;
-//     }
-//   });
